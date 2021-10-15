d0d1297898507b9d2fca21cc75914e905431f600
Halyna-Velychko-Amway <Halyna.Velychko@Amway.com>
2021-10-15 23:22:25 +0300

    [HW-2-Components]: added shared button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ButtonSize } from './enums/button-size.enum';
+import { ButtonType } from './enums/button-type.enum';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent implements OnInit {
+
+export class ButtonComponent  {
+  @Input() buttonType: ButtonType.BLUE | ButtonType.GREEN | ButtonType.TRANSPARENT = ButtonType.TRANSPARENT;
+
+  @Input() buttonSize = ButtonSize.SMALL;
+
+  @Input() buttonText!: string;
+
+  @Input() iconSrc!: string;
 
   constructor() { }
 
-  ngOnInit(): void {
+  public get defaultClass(): string {
+    return `button button--default button--${this.buttonType} button--${this.buttonSize}`
   }
-
 }
---
+++ src/app/shared/components/button/enums/button-type.enum.ts
@@ -0,0 +1,5 @@
+export enum ButtonType {
+  BLUE = 'blue',
+  GREEN = 'green',
+  TRANSPARENT = 'transparent',
+}
