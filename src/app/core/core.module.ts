4a79dae329705f511d71101d7715f8c7c39e0f43
Halyna-Velychko-Amway <Halyna.Velychko@Amway.com>
2021-10-15 23:20:35 +0300

    [HW-2-Components]: added breadcrumbs component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,19 +1,24 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent, FooterComponent } from './components';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
+    BreadcrumbsComponent,
   ],
   imports: [
     CommonModule,
+    SharedModule,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
+    BreadcrumbsComponent,
   ],
 })
 export class CoreModule { }
